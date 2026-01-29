4c9d38e719e608b255e43f2a038db225a9627bb0
Ayobami Adefolalu <tradebridge001@gmail.com>
2026-01-29 09:02:29 +0100

    Add test suite - all passing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -58,6 +59,10 @@ const config: HardhatUserConfig = {
     tests: "./test",
     cache: "./cache",
     artifacts: "./artifacts",
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
   },
 };
 
